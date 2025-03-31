be8056f4e52e9bf8a507a35bdbfecc773c12cd77
Kenneth Huang <kennyh318@gmail.com>
2025-03-31 15:53:49 -0700

    fix spacing

--- selfdrive/ui/sunnypilot/qt/offroad/settings/lateral_panel.h
+++ selfdrive/ui/sunnypilot/qt/offroad/settings/lateral_panel.h
@@ -5,42 +5,42 @@
  * See the LICENSE.md file in the root directory for more details.
  */
 
- #pragma once
+#pragma once
 
- #include <map>
- #include <string>
+#include <map>
+#include <string>
 
- #include "selfdrive/ui/sunnypilot/ui.h"
- #include "selfdrive/ui/sunnypilot/qt/offroad/settings/lateral/mads_settings.h"
- #include "selfdrive/ui/sunnypilot/qt/offroad/settings/lateral/neural_network_lateral_control.h"
- #include "selfdrive/ui/sunnypilot/qt/offroad/settings/lateral/lane_change_settings.h"
- #include "selfdrive/ui/qt/util.h"
- #include "selfdrive/ui/sunnypilot/qt/offroad/settings/settings.h"
- #include "selfdrive/ui/sunnypilot/qt/widgets/scrollview.h"
+#include "selfdrive/ui/sunnypilot/ui.h"
+#include "selfdrive/ui/sunnypilot/qt/offroad/settings/lateral/mads_settings.h"
+#include "selfdrive/ui/sunnypilot/qt/offroad/settings/lateral/neural_network_lateral_control.h"
+#include "selfdrive/ui/sunnypilot/qt/offroad/settings/lateral/lane_change_settings.h"
+#include "selfdrive/ui/qt/util.h"
+#include "selfdrive/ui/sunnypilot/qt/offroad/settings/settings.h"
+#include "selfdrive/ui/sunnypilot/qt/widgets/scrollview.h"
 
- class LateralPanel : public QFrame {
-   Q_OBJECT
+class LateralPanel : public QFrame {
+  Q_OBJECT
 
- public:
-   explicit LateralPanel(SettingsWindowSP *parent = nullptr);
-   void showEvent(QShowEvent *event) override;
-   void hideEvent(QHideEvent* event) override;
+public:
+  explicit LateralPanel(SettingsWindowSP *parent = nullptr);
+  void showEvent(QShowEvent *event) override;
+  void hideEvent(QHideEvent* event) override;
 
- public slots:
-   void updateToggles(bool _offroad);
+public slots:
+  void updateToggles(bool _offroad);
 
- private:
-   QStackedLayout* main_layout = nullptr;
-   QWidget* sunnypilotScreen = nullptr;
-   ScrollViewSP *sunnypilotScroller = nullptr;
-   std::vector<ParamControl *> toggleOffroadOnly;
-   bool offroad;
+private:
+  QStackedLayout* main_layout = nullptr;
+  QWidget* sunnypilotScreen = nullptr;
+  ScrollViewSP *sunnypilotScroller = nullptr;
+  std::vector<ParamControl *> toggleOffroadOnly;
+  bool offroad;
 
-   ParamControl *madsToggle;
-   PushButtonSP *madsSettingsButton;
-   MadsSettings *madsWidget = nullptr;
-   PushButtonSP *laneChangeSettingsButton;
-   LaneChangeSettings *laneChangeWidget = nullptr;
-   NeuralNetworkLateralControl *nnlcToggle = nullptr;
-   ParamControl *speedColorToggle; // New toggle for speed color change
- };
+  ParamControl *madsToggle;
+  PushButtonSP *madsSettingsButton;
+  MadsSettings *madsWidget = nullptr;
+  PushButtonSP *laneChangeSettingsButton;
+  LaneChangeSettings *laneChangeWidget = nullptr;
+  NeuralNetworkLateralControl *nnlcToggle = nullptr;
+  ParamControl *speedColorToggle; // New toggle for speed color change
+};
